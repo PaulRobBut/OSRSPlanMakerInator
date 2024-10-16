96dfa28637ae72558ba4abed214f9a674444ff77
PaulRobBut <paulrob.butler@gmail.com>
2024-10-15 18:31:51 -0700

    Added Project Map, started working on menu navigation.

--- OSRSPMItest.py
+++ OSRSPMItest.py
@@ -13,6 +13,24 @@ class ChanceItem():
         print(self.good_items)
         print(self.bad_items)
 
+progStateStack = ["Main Menu"]
+progNextStates = ["Chance Items", "Inventory", "Main Goals", "Edit"]
+## 0 = Main Menu
+## 1 = Chance Items
+## 2 = Inventory
+## 3 = Main Goals
+## 4 = Edit
+## 5 = Edit Chance Items
+## 6 = Edit Inventory
+## 7 = Edit Main Goals
+## 8 = Load Plans
+## 9 = Save Plans
+
 test = ChanceItem("Baby Impling Jar", 0, 1000, ["Basically", "Nothing"], ["Everything Else"])
 print("Doot")
 test.printChanceItem()
+userChoice = ""
+while len(progStateStack) > 0:
+    userChoice = input("Say Something: ")
+    print(userChoice)
+    print(progStateStack.pop())
