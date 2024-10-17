e3ea26b51429ec1affa358788e248cf0b2ef625e
PaulRobBut <paulrob.butler@gmail.com>
2024-10-17 13:53:08 -0700

    Updated the plan to give myself a to do list, added objects for Inventory Items and Main Goals, and added two fields to Chance Items for needed items and if there's a PK risk.

--- OSRSPMItest.py
+++ OSRSPMItest.py
@@ -5,12 +5,14 @@ progNextStates = ["Chance Items", "Inventory", "Main Goals", "Edit"]
 
 ## Classes
 class ChanceItem():
-    def __init__(self, n = "Commorb", a = 0, p = 1, g = [], b = []):
+    def __init__(self, n = "Commorb", a = 0, p = 1, g = [], b = [], pk = False, i = "None"):
         self.name = n
         self.amount = a
         self.price = p
         self.good_items = g
         self.bad_items = b
+        self.pkRisk = pk
+        self.itemNeeded = i
 
     def printChanceItem(self):
         print(self.name)
@@ -18,6 +20,31 @@ class ChanceItem():
         print(self.price)
         print(self.good_items)
         print(self.bad_items)
+        print(self.pkRisk)
+        print(self.itemNeeded)
+
+class InvenItem():
+    def __init__(self, k, n, a):
+        self.key = k
+        self.name = n
+        self.amount = a
+
+    def compare(self, o): ## o: other item to compare
+        if self.name == o.name:
+            self.amount += o.amount
+            return True
+        else:
+            return False
+
+    def printInvenItem(self):
+        print(self.name + " " + str(self.amount))
+
+
+class MainGoal():
+    def __init__(self, k, n, d):
+        self.key = k
+        self.name = n
+        self.description = d
 
 ## Functions
 def setState(menuChoice, validChoices):
@@ -46,8 +73,22 @@ def setState(menuChoice, validChoices):
 ## 8 = Load Plans
 ## 9 = Save Plans
 
+## Variables
+userChoice = "" ## User input value
+chcArr = [0,0,0,0,0,0,0,0,0,0,0] ## Chance Item Array
+## Key: (Young Impling Jar), (Gourmet Impling Jar), (Eclectic Impling Jar), (Magpie Impling Jar), (Dragon Impling Jar), (Fiyr remains), (Urium Remains), (Ogre Coffin Keys), (Zombie Pirate Keys), (Rogue's Chest), (Grubby Chest)
+
 ## test = ChanceItem("Baby Impling Jar", 0, 1000, ["Basically", "Nothing"], ["Everything Else"])
-userChoice = ""
+
+## Testing Inventory items and comparisons
+## test = InvenItem(0, "Bug", 2)
+## test.printInvenItem()
+## testTwo = InvenItem(0, "Book", 3)
+## print(test.compare(testTwo))
+## testTwo = InvenItem(0, "Bug", 3)
+## print(test.compare(testTwo))
+## test.printInvenItem()
+
 while len(progStateStack) > 0:
     print("You are here: " + progStateStack[len(progStateStack) - 1])
     print("Submenus: ")
